756fba150f3885e00dcc9b8934fb6f7b1a5f849c
Pruthvi <pruthvi.shetty@apty.io>
2023-03-05 02:24:34 +0530

    Adds use

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,51 +2,51 @@ import { useConversionCanvas } from '@components/ConversionCanvas/useConversionC
 import FileInput from '@components/FileInput/FileInput';
 import { Button, Input } from '@hover-design/react';
 import { getImageMetaData } from '@services/image-process';
-import { useJimpImage } from '@services/jimp-service/useJimpImage';
-import { ChangeEventHandler, useState } from 'react';
+import { FitMethod } from '@services/useSqoosh/types';
+import { useSquoosh } from '@services/useSqoosh/useSqoosh';
+
+import { ChangeEventHandler, useEffect, useRef, useState } from 'react';
 import * as styles from './App.css';
 
 function App() {
-    const [fileMeta, setFileMetaData] = useState(null);
-    const onConversion = (img: string) => {
-        console.log(img);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const opts = {
+        resizeOpts: { width: 300, height: 300, fitMethod: FitMethod.Contain }
     };
-    const { convertImage, convertedImage, imageRef } =
-        useConversionCanvas(onConversion);
-    const { loadImage } = useJimpImage();
-    const getBase64 = (file: File) => {
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = () => resolve(reader.result);
-            reader.onerror = (error) => reject(error);
-        });
-    };
-    const handleFileInput: ChangeEventHandler<HTMLInputElement> = async (e) => {
-        if (e?.currentTarget?.files?.length) {
-            const file = e.currentTarget.files[0];
-            const data = await getImageMetaData(file);
-            loadImage(file);
-            // const imageB64 = await getBase64(file);
-            // // imageRef.current.src = imageB64 as string;
-            // // convertImage({
-            // //     canvasRef: imageRef,
-            // //     format: 'webp',
-            // //     quality: 0.5
-            // // });
+
+    const { squooshFile, loading, imgSrcPreview, file } = useSquoosh();
+
+    const onChangeImage = () => {
+        const files = inputRef.current?.files || [];
+        if (files[0]) {
+            squooshFile(files[0], opts);
         }
     };
+
+    useEffect(() => {
+        if (file) {
+            // Do something with the compressed file
+            console.log(file);
+        }
+    }, [file]);
     return (
         <div className={styles.GlobalWrapper}>
             <div className={styles.FileInputSection}>
-                <FileInput onChange={handleFileInput} />
+                {/* <FileInput onChange={handleFileInput} /> */}
+                <input
+                    ref={inputRef}
+                    accept="image/*"
+                    type="file"
+                    onChange={onChangeImage}
+                />
+                {imgSrcPreview && <img src={imgSrcPreview} alt="Preview" />}
             </div>
             <div className={styles.ActionBtnsSection}>
                 <Button>Convert To</Button>
                 <Button>Compress</Button>
                 <Button>Resize</Button>
             </div>
-            <img ref={imageRef} />;
+            {/* <img ref={imageRef} />; */}
         </div>
     );
 }
