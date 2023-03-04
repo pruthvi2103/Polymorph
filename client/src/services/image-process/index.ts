c5e6d959a317484b02f9451c9b697a49861a45be
kumarahul98 <kumar.rahul98k@gmail.com>
2023-03-04 18:17:57 +0530

    Add: canvas module

--- client/src/services/image-process/index.ts
+++ client/src/services/image-process/index.ts
@@ -1,4 +1,29 @@
-import { getMetadata } from 'business_logic';
-export async function getImageMetaData(fileBuffer: Buffer) {
-    return getMetadata(fileBuffer);
+import React, { useState } from 'react';
+enum formatType {
+    'webp',
+    'jpeg',
+    'png'
 }
+const [convertedImage, setConvertedImage] = useState('');
+export const convert = async ({
+    canvas,
+    ctx,
+    canvasRef,
+    onConversion,
+    format,
+    quality
+}: {
+    canvas: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D;
+    canvasRef: React.RefObject<HTMLImageElement>;
+    onConversion: any;
+    format: formatType;
+    quality: any;
+}) => {
+    canvas.width = canvasRef.naturalWidth;
+    canvas.height = canvasRef.naturalHeight;
+    ctx.drawImage(canvasRef as unknown as CanvasImageSource, 0, 0);
+    setConvertedImage(canvas.toDataURL(`image/${format}`, quality));
+
+    setConvertedImage(onConversion(convertedImage));
+};
